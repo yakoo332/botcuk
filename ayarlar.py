91c3c77e9f54852cf6dab0a5d8c38fa368dba156
yakoo332 <yakupcevik2022@gmail.com>
2025-09-07 04:02:29 +0300

    Update ayarlar.py

--- ayarlar.py
+++ ayarlar.py
@@ -5,9 +5,10 @@ api_hash = '27157979acd673f226ebd1c73c57351d'
 
 # Birden fazla kanal/grup dinlemek için liste
 group_ids = [
-    2872688321,     # Eski kanal
-    3005837147, # Yeni eklediğin kanal ID'sis
+    2389959962,     # Eski kanal
+    2918128742, # Yeni eklediğin kanal ID'sis
     ]
 
 bot_token = '8237269791:AAFAe637mmxbSL60S9qfXPrlnNU8CUFB0UM'
 chat_ids_file = 'chat_ids.txt'  # /start diyen kullanıcılar burada tutulacak
+
